8b5d270bb675da8aa0d80aa270733a51ed608288
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2020-03-15 02:54:52 -0400

    added state to the form and added loaders

--- src/features/activities/form/ActivityForm.tsx
+++ src/features/activities/form/ActivityForm.tsx
@@ -8,9 +8,10 @@ interface IProps {
 	activity: IActivity
 	createActivity: (activity: IActivity) => void;
 	editActivity: (activity: IActivity) => void;
+	submitting: boolean;
 }
 
-const ActivityForm: React.FC<IProps> = ({setEditMode, activity: initialFormState, createActivity, editActivity}) => {
+const ActivityForm: React.FC<IProps> = ({setEditMode, activity: initialFormState, createActivity, editActivity, submitting}) => {
 
 	const initializeForm = () => {
 		if (initialFormState) {
@@ -34,7 +35,7 @@ const ActivityForm: React.FC<IProps> = ({setEditMode, activity: initialFormState
 		if (activity.id.length === 0) {
 			let newActivity = {
 				...activity,
-				id: 'guid'
+				id: uuid()
 			}
 			createActivity(newActivity);
 		} else {
@@ -56,7 +57,7 @@ const ActivityForm: React.FC<IProps> = ({setEditMode, activity: initialFormState
 				<Form.Input onChange={handleInputChange} name="date" type="datetime-local" placeholder="Date" value={activity.date}/>
 				<Form.Input onChange={handleInputChange} name="city" placeholder="City" value={activity.city}/>
 				<Form.Input onChange={handleInputChange} name="venue" placeholder="Venue" value={activity.venue}/>
-				<Button floated="right" positive type="submit" content="Submit" />
+				<Button loading={submitting} floated="right" positive type="submit" content="Submit" />
 				<Button onClick={() => setEditMode(false)} floated="right" type="submit" content="Cancel" />
 			</Form>
 		</Segment>
