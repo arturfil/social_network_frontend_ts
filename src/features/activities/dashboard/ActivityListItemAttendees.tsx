49b4f745b57b65ea57923a471b61a36a2f5df6e3
arturfil <arturfil@hotmail.com>
2020-11-16 02:41:22 -0500

    added the join and cancel activity functionality of the application

---
+++ src/features/activities/dashboard/ActivityListItemAttendees.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Image, List, Popup } from 'semantic-ui-react'
+import { IAttendee } from '../../../app/models/activity'
+
+interface IProps {
+  attendees: IAttendee[]
+}
+
+const ActivityListItemAttendees: React.FC<IProps> = ({attendees}) => {
+  return (
+    <List horizontal>
+      {attendees.map(attendee => (
+        <List.Item key={attendee.username}>
+          <Popup 
+            header={attendee.username}
+            trigger={
+              <Image size='mini' circular src={'assets/user.png'}/>
+            }
+          />
+        </List.Item>
+      ))}
+    </List>
+  )
+}
+
+export default ActivityListItemAttendees
--- src/features/activities/details/ActivityDetailedSidebar.tsx
+++ src/features/activities/details/ActivityDetailedSidebar.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import { Item, Label, Image, List, Segment } from 'semantic-ui-react'
 import { Link } from 'react-router-dom';
+import { IAttendee } from '../../../app/models/activity';
+import { observer } from 'mobx-react-lite';
 
-const ActivityDetailedSidebar = () => {
+interface IProps {
+  attendees: IAttendee[];
+}
+
+const ActivityDetailedSidebar: React.FC<IProps> = ({attendees}) => {
+  
   return (
     <>
       <Segment  
@@ -13,51 +20,34 @@ const ActivityDetailedSidebar = () => {
         inverted
         color='teal'
       >
-        3 people going
+        {attendees.length} {attendees.length === 1 ? 'Person' : 'People'} going
       </Segment>
       <Segment attached>
         <List relaxed divided>
-          
-          <Item style={{position: 'relative'}}>
-            <Label
-              style={{position: 'absolute'}}
-              color='orange'
-              ribbon='right'
-            >
-              Host
-            </Label>
-            <Image size='tiny' src={'/assets/user.png'}/>
-            <Item.Content verticalAlign='middle'>
-              <Item.Header as='h3'>
-                <Link to={`#`}>Bob</Link>
-              </Item.Header>
-            </Item.Content>
-          </Item>
-
-          <Item style={{position: 'relative'}}>
-            <Image size='tiny' src={'/assets/user.png'}/>
-            <Item.Content verticalAlign='middle'>
-              <Item.Header as='h3'>
-                <Link to={`#`}>Bob</Link>
-              </Item.Header>
-              <Item.Extra style={{color: 'orange' }}>Following</Item.Extra> 
-            </Item.Content>
-          </Item>
-
-          <Item style={{position: 'relative'}}>
-            <Image size='tiny' src={'/assets/user.png'}/>
-            <Item.Content verticalAlign='middle'>
-              <Item.Header as='h3'>
-                <Link to={`#`}>Bob</Link>
-              </Item.Header>
-              <Item.Extra style={{color: 'orange' }}>Following</Item.Extra> 
-            </Item.Content>
-          </Item>
-
+          {attendees.map(attendee => (
+            <Item key={attendee.username} style={{position: 'relative'}}>
+              {attendee.isHost &&
+                <Label
+                  style={{position: 'absolute'}}
+                  color='orange'
+                  ribbon='right'
+                >
+                  Host
+                </Label>
+              }
+              <Image size='tiny' src={attendee.image||'/assets/user.png'}/>
+              <Item.Content verticalAlign='middle'>
+                <Item.Header as='h3'>
+                  <Link to={`/profile/${attendee.username}`}>{attendee.displayName}</Link>
+                </Item.Header>
+                <Item.Extra style={{color: 'orange' }}>Following</Item.Extra> 
+              </Item.Content>
+            </Item>
+          ))}
         </List>
       </Segment>
     </>
   )
 }
 
-export default ActivityDetailedSidebar
+export default observer(ActivityDetailedSidebar);
