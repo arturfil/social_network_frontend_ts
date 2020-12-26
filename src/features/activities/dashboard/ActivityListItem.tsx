f38fbbaf4fc8b89969efecc8f0b8c315b516f7ac
arturfil <arturfil@hotmail.com>
2020-12-26 16:49:18 -0500

    commiting before running the build production command

--- src/features/activities/dashboard/ActivityListItem.tsx
+++ src/features/activities/dashboard/ActivityListItem.tsx
@@ -6,50 +6,50 @@ import { format } from 'date-fns';
 import ActivityListItemAttendees from './ActivityListItemAttendees';
 
 const ActivityListItem: React.FC<{ activity: IActivity }> = ({ activity }) => {
-    const host = activity.attendees.filter(x => x.isHost)[0];
+  const host = activity.attendees.filter(x => x.isHost)[0];
 
-    return (
-        <Segment.Group>
-            <Segment>
-                <Item.Group>
-                    <Item>
-                        <Item.Image className="circular" size="tiny" circular src={host.image || '/assets/user.png'} style={{ marginBottom: '3px' }} />
-                        <Item.Content>
-                            <Item.Header as={Link} to={`/activities/${activity.id}`}>{activity.title}</Item.Header>
-                            <Item.Description>
-                                Hosted By 
+  return (
+    <Segment.Group>
+      <Segment>
+        <Item.Group>
+          <Item>
+            <Item.Image className="circular" size="tiny" circular src={host.image || '/assets/user.png'} style={{ marginBottom: '3px' }} />
+            <Item.Content>
+              <Item.Header as={Link} to={`/activities/${activity.id}`}>{activity.title}</Item.Header>
+              <Item.Description>
+                Hosted By
                                 <Link to={`/profile/${host.username}`}> {host.displayName}</Link>
-                            </Item.Description>
-                            {activity.isHost &&
-                                <Item.Description>
-                                    <Label basic color='orange' content='You are hosting this' />
-                                </Item.Description>
-                            }
-                            {activity.isGoing && !activity.isHost &&
-                                <Item.Description>
-                                    <Label basic color='green' content='You are going to this activity' />
-                                </Item.Description>
-                            }
-                        </Item.Content>
-                    </Item>
-                </Item.Group>
-            </Segment>
-            <Segment>
-                <Icon name="clock" /> {format(activity.date, 'h:mm a')}
-                <Icon name="marker" /> {activity.venue}, {activity.city}
-            </Segment>
-            <Segment secondary>
-                <ActivityListItemAttendees attendees={activity.attendees} />
-            </Segment>
-            <Segment clearing>
-                <span>{activity.description}</span>
-                <Button as={Link}
-                    to={`/activities/${activity.id}`}
-                    floated="right"
-                    content="View" color="blue" />
-            </Segment>
-        </Segment.Group>
-    )
+              </Item.Description>
+              {activity.isHost &&
+                <Item.Description>
+                  <Label basic color='orange' content='You are hosting this' />
+                </Item.Description>
+              }
+              {activity.isGoing && !activity.isHost &&
+                <Item.Description>
+                  <Label basic color='green' content='You are going to this activity' />
+                </Item.Description>
+              }
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </Segment>
+      <Segment>
+        <Icon name="clock" /> {format(activity.date, 'h:mm a')}
+        <Icon name="marker" /> {activity.venue}, {activity.city}
+      </Segment>
+      <Segment secondary>
+        <ActivityListItemAttendees attendees={activity.attendees} />
+      </Segment>
+      <Segment clearing>
+        <span>{activity.description}</span>
+        <Button as={Link}
+          to={`/activities/${activity.id}`}
+          floated="right"
+          content="View" color="blue" />
+      </Segment>
+    </Segment.Group>
+  )
 }
 
 export default ActivityListItem;
